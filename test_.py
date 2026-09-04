43f51423748d0fbc730910ca2aa8eabe17b8c820
MatthewUnderscoreShen <matthewshen02@gmail.com>
2026-09-03 23:32:55 -0700

    minor

--- test_.py
+++ test_.py
@@ -13,21 +13,28 @@ def test_Bezier():
     p1 = [2, 1, 0]
     r = 0.25
     curve = BezierCurve(p0, p1, r)
-    t = np.linspace(0, 1, 101)          # single 
+
+    t = np.linspace(0, 1, 101)          # t array
     xy = curve.get_bezier(t)
     xy2 = np.zeros((len(t), 2))
-    coeff = curve.get_bezier_coeff()
-    d_test_pt1 = ()
+
+    coeff = curve.get_bezier_coeff()    # bezier coeffs
+
+    d_test_pts = ((0,0), (2,1), (-1000,1000), (1.999,0.999))
+
 
     # Act
-    
     for i in range(len(t)):
         xy2[i,:] = curve.get_bezier(t[i])
     coeff_ans = ((2,-1), (0,2), (0,0))
 
+    d_test_ans = (True, False, False, True)
+
     
     # Assert
-    assert pytest.approx(curve.get_p2()) == [0,1]   # in between point
-    assert np.all(np.equal(xy, xy2))                # single t vs array t
-    for q, a in zip(coeff, coeff_ans):              # d/dt coefficients
+    assert pytest.approx(curve.get_p2()) == [0,1]   # in between point (__init__ test)
+    assert np.all(np.equal(xy, xy2))                # single t vs array t (get_bezier test)
+    for q, a in zip(coeff, coeff_ans):              # d/dt coefficients (get_bezier_coeff test)
         assert pytest.approx(q) == a
+    for q, a in zip(d_test_pts, d_test_ans):
+        assert curve.check_end_distance(q) == a     # (check_end_distance test)
